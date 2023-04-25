ea774df19d9344d93834d314e4f436739e87ca26
Shashvat Maurya <103407023+Shashvat300@users.noreply.github.com>
2023-04-26 01:03:07 +0530

    Update 41_Argument_Pass_by_Value.sv

--- 41_Argument_Pass_by_Value.sv
+++ 41_Argument_Pass_by_Value.sv
@@ -19,3 +19,15 @@ module Function_SV();
       $display("\t Z=%0d",Z);
     end
 endmodule
+
+
+// Result......
+xcelium> source /xcelium20.09/tools/xcelium/files/xmsimrc
+xcelium> run
+	 A=21
+	 B=22
+	 Z=65
+xmsim: *W,RNQUIE: Simulation is complete.
+xcelium> exit
+TOOL:	xrun	20.09-s003: Exiting on Apr 25, 2023 at 15:27:52 EDT  (total: 00:00:02)
+Done
